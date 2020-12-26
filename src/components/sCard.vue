c4e5a5697627b32b618801c7a06cb97899da9e93
sHa <sha@shadoll.dev>
2020-12-26 20:37:08 +0200

    card component

---
+++ src/components/sCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card flat tile :id="name" v-bind="params">
+        <v-card-title class="orange">{{
+            $t("cv." + name + ".title")
+        }}</v-card-title>
+        <v-card-text>
+            <slot></slot>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: "",
+            params: {},
+        },
+        created() {
+        },
+    };
+</script>
