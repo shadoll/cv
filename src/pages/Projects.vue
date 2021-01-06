08e865075c70b8fd5cacd9213b68a6b1b7a6cd2d
sHa <sha@shadoll.dev>
2021-01-06 18:30:10 +0200

    projects page add

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -4,8 +4,11 @@
         <s-app-bar />
 
         <v-main>
-            <v-sheet id="scrolling-techniques" class="overflow-y-auto">
-
+            <v-sheet id="projects" height="100%" class="overflow-y-auto">
+                <v-card class="d-flex justify-center align-center flex-column" height="100%" outlined tile>
+                    <div><clock size="300px" border="0" bg="orange"></clock></div>
+                    <div>In process...</div>
+                </v-card>
             </v-sheet>
         </v-main>
     </simple>
@@ -14,11 +17,13 @@
 <script>
     import sSideBar from "~/components/sSideBar";
     import sAppBar from "~/components/sAppBar";
+    import Clock from "vue-clock2";
 
     export default {
         components: {
             sSideBar,
             sAppBar,
+            Clock,
         },
         data: () => ({
             textcolor: "orange",
@@ -29,17 +34,15 @@
                 title: this.$i18n.t("meta.title"),
             };
         },
-        created() {
-        },
-        methods: {
-
-        },
-        computed: {
-
-        },
+        created() {},
+        methods: {},
+        computed: {},
     };
 </script>
 
 <style>
-
+    .clock .hour {
+        font-family: "Poiret One", Roboto, sans-serif;
+        font-size: 0.8rem;
+    }
 </style>
