55472e06338798d9544e135710c630ff49a9ab84
sHa <sha@shadoll.dev>
2021-01-07 20:21:57 +0200

    theme switcher fix2

--- src/components/ColorSwitcher.vue
+++ src/components/ColorSwitcher.vue
@@ -1,8 +1,8 @@
 <template>
     <v-menu left bottom>
         <template v-slot:activator="{ on, attrs }">
-            <v-btn icon v-bind="attrs" v-on="on" @click="colorChange()" >
-                <v-icon>mdi-theme-light-dark</v-icon>
+            <v-btn icon v-bind="attrs" v-on="on" @click="colorChange()">
+                <v-icon>{{ themeIcon }}</v-icon>
             </v-btn>
         </template>
     </v-menu>
@@ -12,14 +12,24 @@
     import { mdiConsole } from "@mdi/js";
     export default {
         name: "ColorSwitcher",
+        data: () => ({
+            icon: "mdi-theme-light-dark",
+        }),
         methods: {
             colorChange() {
-                let theme = !this.$vuetify.theme.dark
+                let theme = !this.$vuetify.theme.dark;
                 // console.log(theme)
-                this.$vuetify.theme.dark = theme
+                this.$vuetify.theme.dark = theme;
                 // localStorage.setItem('theme', JSON.stringify(theme?'dark':'light'));
                 // localStorage.setItem('theme',"none")
-            },
+            }
         },
+        computed: {
+            themeIcon() {
+                return this.$vuetify.theme.dark
+                    ? "mdi-brightness-7"
+                    : "mdi-brightness-4";
+            }
+        }
     };
 </script>
