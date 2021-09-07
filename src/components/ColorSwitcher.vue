8cad14ff258a622dba33d1d1279d7d5a73234c5e
sHa <sha@shadoll.dev>
2021-09-07 15:44:56 +0300

    build fix

--- src/components/ColorSwitcher.vue
+++ src/components/ColorSwitcher.vue
@@ -25,7 +25,7 @@
                 let theme = !this.$vuetify.theme.dark;
                 this.$vuetify.theme.dark = theme;
                 // if (process.isClient) {
-                if (localStorage !== 'undefined') {
+                if (localStorage) {
                     localStorage.setItem(
                         "theme",
                         JSON.stringify(theme ? "dark" : "light")
@@ -42,7 +42,7 @@
         },
         created() {
             // if (process.isClient) {
-            if (localStorage !== 'undefined') {
+            if (localStorage) {
                 this.$vuetify.theme.dark =
                     localStorage.getItem("theme") == '"dark"' ? true : false;
             }
